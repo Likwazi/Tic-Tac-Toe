35d5d333caeee73c8977dcaca08c286aaa7839d7
likwazi <singalamba@outlook.com>
2024-01-04 23:47:58 +0200

    UPDATE

--- game/src/App.js
+++ game/src/App.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import Square from "./components/Square";
+
 function App() {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState("X");
+  const chooseSquare = (square) => {
+    setBoard(
+      board.map((value, index) => {
+        if (index == square && value == "") {
+          return player;
+        }
+        return value;
+      })
+    );
+
+    if (player == "X") {
+      setPlayer("O");
+    } else {
+      setPlayer("X");
+    }
+  };
   return (
-    <div className="justify-items-center">
-      <div className="text-3xl text-center">hello</div>
+    <div className="flex">
+      <div className="w-screen h-screen flex justify-center items-center">
+        <div className="w-[500px] h-[500px] bg-teal-300 border-4 border-red-500 flex flex-col">
+          <div className="flex flex-col basis-1/3">
+            <Square
+              val={board[0]}
+              chooseSquare={() => {
+                chooseSquare(0);
+              }}
+            />
+            <Square
+              val={board[1]}
+              chooseSquare={() => {
+                chooseSquare(1);
+              }}
+            />
+            <Square
+              val={board[2]}
+              chooseSquare={() => {
+                chooseSquare(2);
+              }}
+            />
+          </div>
+          <div className="flex flex-col basis-1/3"></div>
+          <div className="flex flex-col basis-1/3"></div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ game/src/components/Square.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Square = ({ val, chooseSquare }) => {
+  return <div className="basis-1/3 h-full border-4 border-red-500 cursor-pointer grid place-items-center text-3xl text-black" onClick={chooseSquare}>{val}</div>;
+};
+
+export default Square;
